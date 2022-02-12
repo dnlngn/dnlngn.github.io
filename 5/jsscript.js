53ddf491b2a0e3d357e2ce3ce0c077a40289a2b7
dnlngn <90645863+dnlngn@users.noreply.github.com>
2022-02-12 18:14:42 +0300

    Update jsscript.js

--- 5/jsscript.js
+++ 5/jsscript.js
@@ -1,10 +1,11 @@
 function calc() {
     let p = document.getElementsByName("price");
     let c = document.getElementsByName("count");
+    let a = document.getElementsById("answer");
     if ((/^[1-9][0-9]*$/).test(p[0].value) && (/^[1-9][0-9]*$/).test(c[0].value)) {
-        alert("Ответ: " + parseInt(p[0].value) * parseInt(c[0].value));
+        a.innerHTML = "Ответ: " + parseInt(p[0].value) * parseInt(c[0].value);
     } else {
-        alert("Некорректные данные");
+        a.innerHTML = "Некорректные данные";
     }
 }
 window.addEventListener("DOMContentLoaded", function(event) {
