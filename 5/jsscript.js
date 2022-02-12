0811f4c72a3ec4b3d97e1232088edc2fd1d97fae
dnlngn <90645863+dnlngn@users.noreply.github.com>
2022-02-12 18:22:53 +0300

    Update jsscript.js

--- 5/jsscript.js
+++ 5/jsscript.js
@@ -1,7 +1,7 @@
 function calc() {
     let p = document.getElementsByName("price");
     let c = document.getElementsByName("count");
-    let a = document.getElementsById("answer");
+    let a = document.getElementById("answer");
     if ((/^[1-9][0-9]*$/).test(p[0].value) && (/^[1-9][0-9]*$/).test(c[0].value)) {
         a.innerHTML = "Ответ: " + parseInt(p[0].value) * parseInt(c[0].value);
     } else {
