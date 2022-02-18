e175ed6c144fd0c234aca4f928c2645d532fbde3
dnlngn <90645863+dnlngn@users.noreply.github.com>
2022-02-18 17:24:39 +0300

    Update jsscript.js

--- 6/jsscript.js
+++ 6/jsscript.js
@@ -1,4 +1,4 @@
-function calc() {
+function calc1() {
     let p = document.getElementsByName("price");
     let c = document.getElementsByName("count");
     let a = document.getElementById("answer");
@@ -10,88 +10,76 @@ function calc() {
     return false;
 }
 
+function hiden() {
+    let prod;
+    prod = document.getElementById('prod').value;
+    switch (prod) {
+        case'1':
+            document.getElementById("radio").hidden = true;
+            document.getElementById("checkbox").hidden = true;
+            break;
+        case'2':
+            document.getElementById("radio").hidden = false;
+            document.getElementById("checkbox").hidden = true;
+            break;
+        case'3':
+            document.getElementById("radio").hidden = true;
+            document.getElementById("checkbox").hidden = false;
+            break;
+    }
+}
+function calc2() {
+        let t2 = /^\d+$/;
+        let prod, count, checkbox, radio, answer;
+        let result = 0;
+        prod = document.getElementById('prod').value;
+        count = parseInt(document.getElementById('count').value);
+        checkbox = document.getElementsByName('checkbox');
+        radio = document.getElementsByName('radio');
+        answer = document.getElementById('answer');
+        if (!t2.test(count) || (count < 0)) {
+            answer.innerHTML = "Введите количество";
+        } else {
+            switch (prod) {
+                case '1':
+                    result = count * 300;
+                    break;
+                case '2':
+                    if (radio[0].checked) {
+                        result = count * 100;
+                    }
+                    if (radio[1].checked) {
+                        result = count * 7000;
+                    }
+                    break;
+                case '3':
+                    result = count * 400;
+                    if (checkbox[0].checked) {
+                        result += count * 200;
+                    }
+                    if (checkbox[1].checked) {
+                        result += count * 100;
+                    }
+                    break;
+            }
+            answer.innerHTML = result + " руб.";
+        }
+    } 
+
+
+
 window.addEventListener('DOMContentLoaded', function(event) {
-    console.log("DOM fully loaded and parsed");
-    let b = document.getElementById("result");
-    b.addEventListener("click", calc);
 });
 
-        let volume = document.getElementById("volume");
-        let rolls = document.getElementById("rolls");
-        let volumeVal = document.querySelectorAll('input[name="volume"]');
-        let additVal = document.querySelectorAll('input[name="addition"]');
-        let result = document.getElementsByClassName("result");
-        let c = document.getElementsByName("colvo");
-        let onePrice = 0;
-        volume.style.display = "none";
-        rolls.style.display = "none";
-        c[0].disabled = true;
-
-        let s = document.getElementsByName("myselect");
-        s[0].addEventListener("change", function (event) {
-            let select = event.target;
-            if (select.value == "1") {
-                volume.style.display = "none";
-                rolls.style.display = "none";
-                result[0].innerHTML = 450 * c[0].value;
-                onePrice = 450;
-                c[0].disabled = false;
-            }
-            else if (select.value == "2") {
-                volume.style.display = "block";
-                rolls.style.display = "none";
-                result[0].innerHTML = "";
-                for (const vol of volumeVal) {
-                    if(vol.checked){
-                        vol.checked = false;
-                    }
-                }
-                c[0].disabled = true;
-            }
-            else {
-                volume.style.display = "none";
-                rolls.style.display = "block";
-                result[0].innerHTML = 600 * c[0].value;
-                onePrice = 600;
-                c[0].disabled = false;
-            }
-        });
-
-        volumeVal.forEach(function(radio) {
-            radio.addEventListener("change", function(event) {
-              let r = event.target;
-              c[0].disabled = false;
-              result[0].innerHTML = r.value * c[0].value;
-              onePrice = r.value;
-            });    
-          });
-
-        c[0].addEventListener("input", function(event) {
-            let col = event.target;
-             if(/^[1-9]\d*$/.test(col.value)){
-                 result[0].innerHTML = onePrice * col.value;
-            }
-            else{
-                if(col.value != "")
-                    alert("Введите корректные данные!");
-                result[0].innerHTML = col.value = "";
-            }
-            
-          });    
-
-          additVal.forEach(function(checkbox) {
-            checkbox.addEventListener("change", function(event) {
-              let add = event.target;
-              if(add.checked)
-              {
-                result[0].innerHTML = (onePrice + parseInt(add.value)) * c[0].value;
-                onePrice = onePrice + parseInt(add.value);
-              }
-              else
-              {
-                result[0].innerHTML = (onePrice - parseInt(add.value)) * c[0].value;
-                onePrice = onePrice - parseInt(add.value);
-              }
-            });    
-          });
-    });
+  window.addEventListener('DOMContentLoaded', function (event) {
+    console.log("DOM fully loaded and parsed");
+    let b = document.getElementById("result");
+    b.addEventListener("click", calc1);
+    document.getElementById("prod").addEventListener("click", hiden);
+    document.getElementById("count").addEventListener("change", calc2);
+    document.getElementById("prod").addEventListener("change", calc2);
+    document.getElementById("r1").addEventListener("change", calc2);
+    document.getElementById("r2").addEventListener("change", calc2);
+    document.getElementById("chk1").addEventListener("change", calc2);
+    document.getElementById("chk2").addEventListener("change", calc2);
+  });
